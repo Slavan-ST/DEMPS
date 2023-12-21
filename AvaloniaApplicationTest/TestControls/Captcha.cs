2e96ad0d208d24c2f5fe865ff649c35eae51d884
= <slavan990900000@gmail.com>
2023-12-21 17:48:20 +0700

    fix
    итак, я на верном пути.
    Осталось В стиле подправить  капчу/её своййства, а потом в новый класс добавить все свойства

---
+++ AvaloniaApplicationTest/TestControls/Captcha.cs
@@ -0,0 +1,43 @@
+﻿using Avalonia.Controls.Metadata;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Data;
+
+namespace AvaloniaApplicationTest.TestControls
+{
+    [TemplatePart("CaptchaPresenter", typeof(ItemsControl))]
+    public class Captcha:TemplatedControl
+    {
+
+        public Captcha()
+        {
+
+        }
+
+
+        // Мы переопределяем OnPropertyChanged базового класса. Таким образом, мы можем реагировать на изменения свойств
+        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+        {
+            base.OnPropertyChanged(change);
+        }
+
+        // Мы переопределяем то, что происходит при применении шаблона элемента управления.
+        // Таким образом, мы можем, например, прослушивать события элементов управления, которые являются частью шаблона
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+        {
+            base.OnApplyTemplate(e);
+        }
+
+        //Вызывается для обновления состояния проверки свойств, для которых включена проверка данных
+        protected override void UpdateDataValidation(AvaloniaProperty property, BindingValueType state, Exception? error)
+        {
+            base.UpdateDataValidation(property, state, error);
+        }
+    }
+}
