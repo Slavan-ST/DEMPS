f97bdfb31b9e6e4af6eda91732c418e721b1da0b
= <slavan990900000@gmail.com>
2023-12-26 00:19:25 +0700

    restruct
    ещё немного перебрал проект, так будет лучше

---
+++ DEMPS/AvaloniaApplicationTest/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using DEMPS.Services;
+using MsBox.Avalonia;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System.Windows.Input;
+using System;
+using System.Diagnostics;
+
+namespace AvaloniaApplicationTest.ViewModels;
+
+public class MainViewModel : ReactiveObject
+{
+    public MainViewModel()
+    { }
+}
